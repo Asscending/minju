83b334ed7a9f9c4cfd45b9d10e774b3c95cb3bda
Asencding <ascending77@naver.com>
2020-03-31 16:43:06 +0900

    컨트롤러 수정 추가

--- src/main/java/com/example/demo/controller/DeptController.java
+++ src/main/java/com/example/demo/controller/DeptController.java
@@ -1,27 +1,39 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dao.DeptDao;
 import com.example.demo.vo.DeptVo;
+import com.google.gson.Gson;
 @RestController
 public class DeptController {
-//	DeptDao dao = new DeptDao();
+	
+	@Autowired
+	private DeptDao dao;
+	
+	public void setDao(DeptDao dao) {
+		this.dao = dao;
+	}
+
 	@RequestMapping("/insertDept")
 	public String insertDept(DeptVo d) {
 		String r="등록실패";
-/*		int re=dao.insert(d);
+		int re=dao.insertDept(d);
 		if(re>0) {
 			r="등록성공";
-		}*/
+		}
 		return r;
 	}
 	
 	@RequestMapping("/listDept")
 	public String listDept() {
 		String r="";
-/*		List<DeptVo> list = dao.list();
-		r = new Gson().toJson(list);*/
+		List<DeptVo> list = dao.listDept();
+		r = new Gson().toJson(list);
 		return r;
 	}
 }
--- src/main/java/com/example/demo/controller/EmpController.java
+++ src/main/java/com/example/demo/controller/EmpController.java
@@ -1,19 +1,28 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dao.EmpDao;
 import com.example.demo.vo.EmpVo;
 import com.google.gson.Gson;
 
 @RestController
 public class EmpController {
+	
+	@Autowired
+	private EmpDao dao;
+
+	public void setDao(EmpDao dao) {
+		this.dao = dao;
+	}
 
 	@RequestMapping(value="/listEmp", produces = "application/json;charset=UTF-8")
 	public String listEmp() {
 		String str = "";
 		Gson gson = new Gson();
-		//str = gson.toJson(/*dao.listEmp(e)*/);
+		str = gson.toJson(dao.listEmp());
 		return str;
 	}
 	
@@ -21,44 +30,44 @@ public class EmpController {
 	public String detailEmp(EmpVo e) {
 		String str = "";
 		Gson gson = new Gson();
-		//str = gson.toJson(/*dao.detailEmp(e)*/);
+		str = gson.toJson(dao.detailEmp(e));
 		return str;
 	}
 	
 	@RequestMapping("/insertEmp")
 	public String insertEmp(EmpVo e) {
 		String str = "사원 등록에 실패했습니다.";
-		/*
-		e.setEno(dao.nextNo());
+
+		e.setEno(dao.nextEno());
 		int re = dao.insertEmp(e);
 		if( re > 0 ) {
 			str = "사원 등록에 성공했습니다.";
 		}
-		*/
+
 		return str;
 	}
 	
 	@RequestMapping("/updateEmp")
 	public String updateEmp(EmpVo e) {
 		String str = "사원 정보 수정에 실패했습니다.";
-		/*
+		
 		int re = dao.updateEmp(e);
 		if( re > 0 ) {
 			str = "사원 정보 수정에 성공했습니다.";
 		}
-		*/
+		
 		return str;
 	}
 	
 	@RequestMapping("/deleteEmp")
 	public String deleteEmp(EmpVo e) {
 		String str = "사원 정보 삭제에 실패했습니다.";
-		/*
+		
 		int re = dao.deleteEmp(e);
 		if( re > 0 ) {
 			str = "사원 삭제에 성공했습니다.";
 		}
-		*/
+		
 		return str;
 	}
 	
