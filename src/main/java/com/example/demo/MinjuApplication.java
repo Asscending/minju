993fc90740c37d9f0ac6f8587f2ae105b4acb556
Asencding <ascending77@naver.com>
2020-03-31 15:53:39 +0900

    김민주주의조 첫 팀플

---
+++ src/main/java/com/example/demo/MinjuApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinjuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinjuApplication.class, args);
+	}
+
+}
