84719f6baf3b65c08a6d0c25d3e1d613ae71540d
cha2eun <jan9chae2un@gmail.com>
2020-03-31 16:16:11 +0900

    장채은/DeptController

--- src/main/java/com/example/demo/controller/DeptController.java
+++ src/main/java/com/example/demo/controller/DeptController.java
@@ -1,5 +1,25 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class DeptController {
-
+	DeptDao dao = new DeptDao();
+	@RequestMapping("/insertDept")
+	public String insertDept(DeptVo d) {
+		String r="등록실패";
+		int re=dao.insert(d);
+		if(re>0) {
+			r="등록성공";
+		}
+		return r;
+	}
+	
+	@RequestMapping("/listDept")
+	public String listDept() {
+		String r="";
+		List<DeptVo> list = dao.list();
+		r = new Gson().toJson(list);
+		return r;
+	}
 }
