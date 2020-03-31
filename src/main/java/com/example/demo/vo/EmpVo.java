ec7d80340b79092439621464b98444595963b600
Asencding <ascending77@naver.com>
2020-03-31 16:03:50 +0900

    다시올림(유진탁)

---
+++ src/main/java/com/example/demo/vo/EmpVo.java
@@ -0,0 +1,23 @@
+package com.example.demo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+
+public class EmpVo {
+	private int eno;
+	private String ename;
+	private String job;
+	private int sal;
+	private int comm;
+	private int mgr;
+	private String hiredate;
+	private int dno;
+	private String email;
+	private String jumin;
+
+}
